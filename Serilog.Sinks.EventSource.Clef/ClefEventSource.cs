4a2aa6ecf07eef04d1c07db77cd27fe1192029c0
Richard Webb <webby@beardmouse.co.uk>
2020-08-23 16:38:33 +0100

    initial commit

---
+++ Serilog.Sinks.EventSource.Clef/ClefEventSource.cs
@@ -0,0 +1,49 @@
+﻿namespace Serilog.Sinks.EventSource.Clef
+{
+    using System.Diagnostics.Tracing;
+    using Serilog.Events;
+
+    /// <summary>
+    /// An event source that we write CLEF formatted strings to.
+    /// </summary>
+    [EventSource(Name = "Serilog-Sinks-EventSource-Clef")]
+    public sealed class ClefEventSource : EventSource
+    {
+        /// <summary>
+        ///  The one and only instance of the LoggingEventSource.
+        /// </summary>
+        internal static readonly ClefEventSource Instance = new ClefEventSource();
+
+        // Check if the source is enabled at the specified level.
+        [NonEvent]
+        internal bool IsEnabled(LogEventLevel logEventLevel)
+        {
+            switch (logEventLevel)
+            {
+                case LogEventLevel.Fatal:
+                    return IsEnabled(EventLevel.Critical, EventKeywords.None);
+                case LogEventLevel.Error:
+                    return IsEnabled(EventLevel.Error, EventKeywords.None);
+                case LogEventLevel.Warning:
+                    return IsEnabled(EventLevel.Warning, EventKeywords.None);
+                case LogEventLevel.Information:
+                    return IsEnabled(EventLevel.Informational, EventKeywords.None);
+                case LogEventLevel.Debug:
+                    return IsEnabled(EventLevel.Verbose, EventKeywords.None);
+                case LogEventLevel.Verbose:
+                    return IsEnabled(EventLevel.Verbose, EventKeywords.None);
+                default:
+                    return true; // @@TBD@@ On or off by default?
+            }
+        }
+
+        // We only define a single event function, and specify the level as 'always'
+        // could think about different events for different levels later, but for now we'll
+        // just depend on the level stored in the JSON payload.
+        [Event(1, Level = EventLevel.LogAlways)]
+        internal void Message(string clefPayload)
+        {
+            WriteEvent(1, clefPayload);
+        }
+    }
+}
---
+++ Serilog.Sinks.EventSource.Clef/EventSourceClefSink.cs
@@ -0,0 +1,57 @@
+﻿namespace Serilog.Sinks.EventSource.Clef
+{
+    using System;
+    using System.IO;
+    using Serilog.Configuration;
+    using Serilog.Core;
+    using Serilog.Events;
+    using Serilog.Formatting;
+    using Serilog.Formatting.Compact;
+
+    /// <summary>
+    /// An implementation of ILogEventSink that formats events using CompactJsonFormatter and writes them to an EventSource/
+    /// </summary>
+    public sealed class EventSourceClefSink : ILogEventSink
+    {
+        private readonly ITextFormatter textFormatter = new CompactJsonFormatter();
+
+        /// <inheritdoc/>
+        void ILogEventSink.Emit(LogEvent logEvent)
+        {
+            if (ClefEventSource.Instance.IsEnabled(logEvent.Level))
+            {
+                // @@TBD@@ Make this thread/async local so we don't need piles of them?
+                string message = null;
+                using (var writer = new StringWriter())
+                {
+                    textFormatter.Format(logEvent, writer);
+                    message = writer.ToString();
+                }
+
+                ClefEventSource.Instance.Message(message);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Extensions for setting up the sink.
+    /// </summary>
+    public static class ClefLoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Adds the event source sink to the specified LoggerSinkConfiguration.
+        /// </summary>
+        /// <param name="sinkConfiguration">The sink configuration being created.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration ClefSource(
+            this LoggerSinkConfiguration sinkConfiguration,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+
+            var sink = new EventSourceClefSink();
+            return sinkConfiguration.Sink(sink, restrictedToMinimumLevel, null);
+        }
+    }
+}
